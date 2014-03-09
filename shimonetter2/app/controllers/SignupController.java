dc93beadfc5bf6a60b627e33665347d101e8921f
diodario <machimura.akira@gmail.com>
2014-03-09 22:18:39 +0900

    Re-add

---
+++ shimonetter2/app/controllers/SignupController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.data.*;
+import static play.data.Form.*;
+import models.User;
+import play.mvc.*;
+import views.html.*;
+import views.html.helper.*;
+
+public class SignupController extends Controller {
+	public static Result execSignup(){
+		Form<User> form = form(User.class).bindFromRequest();
+		if(!form.hasErrors()){
+			User userData = form.get();
+			userData.save();
+			return redirect("/index");
+		}else{
+			return badRequest(signup.render("ERROR", form));
+		}
+	}
+	
+	public static Result showSignup(){
+		
+		return ok(signup.render("サインアップしようぜ", new Form(User.class)));
+	}
+}
+
